687cf093a4f4730d6f50914b03c98374f9b77369
Bunin Nikita <dancinwhale@yandex.ru>
2024-06-14 16:31:01 +0300

    Added the "News" model and output of articles from the DB

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class News(models.Model):   # Здесь будут новостные статьи, которые будем отображать на сайте
+    title = models.CharField('Title name', max_length=100, unique=True)
+    text = models.TextField('Main title text')
+    date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
--- myApp/views.py
+++ myApp/views.py
@@ -1,24 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 # Create your views here.
 
 
 def home(request):
-    news = [
-        {
-            'title': 'First title',
-            'text': 'Full title text',
-            'date': '10.06.2020',
-            'author': 'Joanna'
-        },
-        {
-            'title': 'Second title',
-            'text': 'Full title text',
-            'date': '30.08.2020'
-        }
-    ]
     data = {
-        'news': news,
+        'news': News.objects.all(),
         'title': 'Main page'
     }
     return render(request, 'myApp/home.html', data)
