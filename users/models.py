98ea96a34f2dc7ec02eae9e8907d095d91637db7
Bunin Nikita <dancinwhale@yandex.ru>
2024-06-16 15:49:46 +0300

    Added profile creation and user photo upload

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    img = models.ImageField(
+        'User photo',
+        default='default_user_photo.png',
+        upload_to='user_images'
+    )
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"
